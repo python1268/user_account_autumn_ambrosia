35ad71604cfbb277fbfd95be702c14c1237215c7
python1268 <pc12425@pc.student.tis.edu.my>
2025-09-12 14:05:50 +0800

    Update receive_order.py

--- receive_order.py
+++ receive_order.py
@@ -385,8 +385,9 @@ def gspread_error():
 def wake_up():
    return "Wake"
 
-if __name__ == "__main__":
-   app.run(debug=True, host="0.0.0.0")
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 while True: 
     load_data()
