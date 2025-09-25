9d1b522f2a514f25af1f522c0e8a3dcf6d1c61d8
python1268 <pc12425@pc.student.tis.edu.my>
2025-09-26 07:39:38 +0800

    Update receive_order.py

--- receive_order.py
+++ receive_order.py
@@ -12,9 +12,10 @@ import time
 import os
 import threading
 
+
 from upstash_redis import Redis
 
-r = Redis(url="https://enormous-mastodon-11551.upstash.io", token="********")
+redis = Redis(url="https://enormous-mastodon-11551.upstash.io", token="Ai0fAAIgcDL23TK4rWf-q8NdZupIGB-TT12w74DFiOOo87y559G8Bg")
 
 
 
