8e4e62b9c5f8c17b84b8ef8bfff5cb3e5f12749a
python1268 <pc12425@pc.student.tis.edu.my>
2025-10-01 16:54:17 +0800

    Update order_2.py

--- order_2.py
+++ order_2.py
@@ -216,7 +216,7 @@ def submit_order():
 #After receive success messages, then show the GET 
 @csrf.exempt
 @app.route("/confirm",methods=["GET","POST"])
-@limiter.limit("110 per hour")
+@limiter.limit("95 per hour")
 def confirm():
     token = request.args.get("token")
     if not token or not r.exists(token):
@@ -301,7 +301,7 @@ def confirm():
                              sheet_customer_cash2.append_row([sanitize_for_sheet(orderdata["customer"]),sanitize_for_sheet(order_summary),sanitize_for_sheet(email),sanitize_for_sheet(userclass),sanitize_for_sheet(phone_num),total])
 
                            email_data = {"order": orderdata["order"], "email": email}
-                           response = req.post("https://script.google.com/macros/s/AKfycbxqeU1Xxzb4ktlnu1BoSvjYk0O3uwnCAP3UVB4SH6kPX3BZMPWQFTMsXGnSadTavmuw/exec", json=email_data, headers={'Content-Type':'application/json'})
+                           response = req.post("https://script.google.com/macros/s/AKfycbyeiJ4K2Kwntw-D_ZnzdtPSejbBnCgC8rUFT2q3rBSMLKEFhELuUo-5pfLE0XgD5n73/exec", json=email_data, headers={'Content-Type':'application/json'})
                            print(response.status_code)
                            r.delete(token)
                         except Exception as e:
@@ -315,7 +315,7 @@ def confirm():
                                    sheet_customer_tng2.append_row([sanitize_for_sheet(orderdata["customer"]),sanitize_for_sheet(order_summary),sanitize_for_sheet(email),sanitize_for_sheet(userclass),sanitize_for_sheet(phone_num), sanitize_for_sheet(transaction_name), total])
                                      
                              email_data = {"order": orderdata["order"], "email": email}
-                             response = req.post("https://script.google.com/macros/s/AKfycbxqeU1Xxzb4ktlnu1BoSvjYk0O3uwnCAP3UVB4SH6kPX3BZMPWQFTMsXGnSadTavmuw/exec", json=email_data, headers={'Content-Type':'application/json'})
+                             response = req.post("https://script.google.com/macros/s/AKfycbyeiJ4K2Kwntw-D_ZnzdtPSejbBnCgC8rUFT2q3rBSMLKEFhELuUo-5pfLE0XgD5n73/exec", json=email_data, headers={'Content-Type':'application/json'})
                              print(response.status_code)
                              r.delete(token)
                             except Exception as e:
