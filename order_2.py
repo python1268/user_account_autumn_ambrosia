39ebdd11755317f80d40bb4b08872d5abd292b63
python1268 <pc12425@pc.student.tis.edu.my>
2025-10-05 14:46:43 +0800

    Update order_2.py

--- order_2.py
+++ order_2.py
@@ -296,10 +296,6 @@ def confirm():
                              sheet_customer_cash.append_row([sanitize_for_sheet(orderdata["customer"]),sanitize_for_sheet(order_summary),sanitize_for_sheet(email),sanitize_for_sheet(userclass),sanitize_for_sheet(phone_num),total])
                            else:
                              sheet_customer_cash2.append_row([sanitize_for_sheet(orderdata["customer"]),sanitize_for_sheet(order_summary),sanitize_for_sheet(email),sanitize_for_sheet(userclass),sanitize_for_sheet(phone_num),total])
-
-                           email_data = {"order": orderdata["order"], "email": email}
-                           response = req.post("https://script.google.com/macros/s/AKfycbyOC_-Kn-DxY3FSKrQBMqX_qikVrxz0MwXpEL_KqQnw8-IUPHeAqCWXwnULbbIrILan/exec", json=email_data, headers={'Content-Type':'application/json'})
-                           print(response.status_code)
                            r.delete(token)
                         except Exception as e:
                             return f"Error in confirm: {str(e)}" 
@@ -310,10 +306,6 @@ def confirm():
                                   sheet_customer_tng.append_row([sanitize_for_sheet(orderdata["customer"]),sanitize_for_sheet(order_summary),sanitize_for_sheet(email),sanitize_for_sheet(userclass),sanitize_for_sheet(phone_num), sanitize_for_sheet(transaction_name), total])
                              else: 
                                    sheet_customer_tng2.append_row([sanitize_for_sheet(orderdata["customer"]),sanitize_for_sheet(order_summary),sanitize_for_sheet(email),sanitize_for_sheet(userclass),sanitize_for_sheet(phone_num), sanitize_for_sheet(transaction_name), total])
-                                     
-                             email_data = {"order": orderdata["order"], "email": email}
-                             response = req.post("https://script.google.com/macros/s/AKfycbyOC_-Kn-DxY3FSKrQBMqX_qikVrxz0MwXpEL_KqQnw8-IUPHeAqCWXwnULbbIrILan/exec", json=email_data, headers={'Content-Type':'application/json'})
-                             print(response.status_code)
                              r.delete(token)
                             except Exception as e:
                               return f"Error in confirm: {str(e)}"
